0b2a0cb4f7df71c2263f74ab09df473886ce76ec
sai94-nova <47029312+sai94-nova@users.noreply.github.com>
2019-07-08 11:34:01 +0530

    Add files via upload

---
+++ Sample/Initial.py
@@ -0,0 +1,8 @@
+print("Hello World!!")
+print("Welcome to Devops Class:")
+print("Running on AWS -> Jenkins")
+print("Creating a Docker and installing python3 and running this file")
+
+sam = input("Enter your Name: ")
+print("Welcome {}, You have succesfully completed your 1st project".format(sam))
+
