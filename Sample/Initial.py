f071d43c9b840bf94a6fccfccf7896a630bed5bc
sai94-nova <47029312+sai94-nova@users.noreply.github.com>
2019-07-08 12:09:41 +0530

    Update Initial.py

--- Sample/Initial.py
+++ Sample/Initial.py
@@ -1,8 +1,8 @@
 print("Hello World!!")
 print("Welcome to Devops Class:")
 print("Running on AWS -> Jenkins")
-print("Creating a Docker and installing python3 and running this file")
+print("Creating a Docker")
 
-sam = input("Enter your Name: ")
-print("Welcome {}, You have succesfully completed your 1st project".format(sam))
+#sam = input("Enter your Name: ")
+#print("Welcome {}, You have succesfully completed your 1st project".format(sam))
 
